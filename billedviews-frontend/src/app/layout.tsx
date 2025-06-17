8d1f104e9c61500456e7d9356d2bcc5fa7bd0f24
JakeKantor <jacobmk8@gmail.com>
2025-06-17 04:17:24 -0400

    Added App Analytics for Vercel

--- billedviews-frontend/src/app/layout.tsx
+++ billedviews-frontend/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
