b2c7c8e4de9de4c44323cc3904c6f83c507bfc3a
JakeKantor <jacobmk8@gmail.com>
2025-06-20 17:44:26 -0400

    Added 4 steps to start earning

--- billedviews-frontend/src/app/why-billedviews/influencers/page.tsx
+++ billedviews-frontend/src/app/why-billedviews/influencers/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/why-billedviews/Header";
 import Footer from "@/components/why-billedviews/Footer";
 import Hero from "@/components/why-billedviews/influencers/Hero";
 import WhyBilledViews from "@/components/why-billedviews/influencers/WhyBilledViews";
+import HowItWorksSection from "./components/HowItWorksSection";
 
 export default function InfluencersPage() {
   return (
@@ -9,6 +10,7 @@ export default function InfluencersPage() {
       <Header />
       <Hero />
       <WhyBilledViews />
+      <HowItWorksSection />
       <Footer />
     </>
   );
