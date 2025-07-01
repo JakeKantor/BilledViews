a51bfc191eeca1dcc17e5038c1f69fe40af57147
JakeKantor <jacobmk8@gmail.com>
2025-07-01 18:18:54 -0400

    Added SpringBoot Backend

---
+++ billedviews-backend/src/main/java/com/billedviews/billedviewsbackend/BilledviewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.billedviews.billedviewsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilledviewsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilledviewsBackendApplication.class, args);
+    }
+
+}
